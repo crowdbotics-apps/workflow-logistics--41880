ac207754f1d7e0a2c0bc2166424e1ea82348be4e
Crowdbotics <team@crowdbotics.com>
2023-07-03 17:19:14 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "workflow-logistics--41880",
+  url: "https://workflow-logistics--41880.botics.co",
+  api: "https://workflow-logistics--41880.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
